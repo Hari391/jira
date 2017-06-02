fd1ad9a7497bace72b162a59b14f53673127169c
Hari391 <hari.harithunder391@gmail.com>
2017-06-02 12:56:08 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,8 @@ module.exports = function (app, addon) {
      // Root route. This route will serve the `atlassian-connect.json` unless the
      // documentation url inside `atlassian-connect.json` is set
     
-     app.get('/', function (req, res) {
-         res.format({
-             // If the request content-type is text-html, it will decide which to serve up
-             'text/html': function () {
-                 res.redirect('/atlassian-connect.json');
-             },
-             // This logic is here to make sure that the `atlassian-connect.json` is always
-             // served up when requested by the host
-             'application/json': function () {
-                 res.redirect('/atlassian-connect.json');
-             }
-         });
+     app.get('/installed', function (req, res) {
+        res.status(200);
      });
 
      // The following is stub code for a Hello World app provided by ACE.
